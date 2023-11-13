13e5d7659038ffb53a232785d7a4a379cc74655c
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2023-11-13 17:03:50 -0500

    obtenerProductos

--- controllers/productoControllers.js
+++ controllers/productoControllers.js
@@ -12,3 +12,15 @@ exports.crearProducto = async (req, res) => {
         res.status(500).send('Hubo un error');
     }
 }
+
+exports.obtenerProductos = async (req, res) => {
+
+    try{
+        const productos = await Producto.find();
+        res.json(producto);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
